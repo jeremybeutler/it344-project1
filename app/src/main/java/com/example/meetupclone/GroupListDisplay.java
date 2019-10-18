84cca83ed3efaf6dee370326bc8d3335ca9351c7
Jeremy Beutler <jeremy.e.beutler@gmail.com>
2019-10-18 17:49:01 -0600

    background request for events

--- app/src/main/java/com/example/meetupclone/GroupListDisplay.java
+++ app/src/main/java/com/example/meetupclone/GroupListDisplay.java
@@ -1,20 +1,12 @@
 package com.example.meetupclone;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class GroupListDisplay extends Activity {
     // Array of strings...
@@ -32,44 +24,43 @@ public class GroupListDisplay extends Activity {
         ListView listView = (ListView) findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
 
-        String urlString = "http://localhost:3000/events";
-        try{
-            JSONObject jsonObject = getJSONObjectFromURL(urlString);
+        String urlString = "http://10.0.2.2:3000/events";
+//        try{
+            RetrieveEvents retrieveEvents = new RetrieveEvents();
+            retrieveEvents.execute(urlString);
             //
             // Parse your json here
             //
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 
-    public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
-        HttpURLConnection urlConnection = null;
-        URL url = new URL(urlString);
-        urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setReadTimeout(10000 /* milliseconds */ );
-        urlConnection.setConnectTimeout(15000 /* milliseconds */ );
-        urlConnection.setDoOutput(true);
-        urlConnection.connect();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line + "\n");
-        }
-        br.close();
-
-        String jsonString = sb.toString();
-        System.out.println("JSON: " + jsonString);
-
-        return new JSONObject(jsonString);
-    }
+//    public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
+//        HttpURLConnection urlConnection = null;
+//        URL url = new URL(urlString);
+//        urlConnection = (HttpURLConnection) url.openConnection();
+//        urlConnection.setRequestMethod("GET");
+//        urlConnection.setReadTimeout(10000 /* milliseconds */ );
+//        urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+//        urlConnection.setDoOutput(true);
+//        urlConnection.connect();
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+//        StringBuilder sb = new StringBuilder();
+//
+//        String line;
+//        while ((line = br.readLine()) != null) {
+//            sb.append(line + "\n");
+//        }
+//        br.close();
+//
+//        String jsonString = sb.toString();
+//        System.out.println("JSON: " + jsonString);
+//
+//        return new JSONObject(jsonString);
+//    }
 }
 
 
---
+++ app/src/main/java/com/example/meetupclone/RetreiveEvents.java
@@ -0,0 +1,53 @@
+package com.example.meetupclone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import android.os.AsyncTask;
+
+class RetrieveEvents extends AsyncTask<String, String, String> {
+
+    private Exception exception;
+
+    protected String doInBackground(String... args) {
+        try {
+            HttpURLConnection urlConnection = null;
+            String url_string = args[0];
+            URL url = new URL(url_string);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+
+            String jsonString = sb.toString();
+            return jsonString;
+        } catch (Exception e) {
+//            Log.d(TAG, e.getLocalizedMessage());
+            System.out.println("Exception: " + e.getLocalizedMessage());
+            return  e.getLocalizedMessage();
+
+        }
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+    }
+
+    protected void onPostExecute(String result) {
+        // this is executed on the main thread after the process is over
+        // update your UI here
+        System.out.println("JSON string result: " + result);
+    }
+}
